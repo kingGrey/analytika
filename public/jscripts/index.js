d97ae49698727108f65faea6b108c13e9c5f81af
kingGrey <greyjohnson@gmail.com>
2021-07-06 17:50:42 -0700

    + updated README.md + adding interface for the application   * interface is not yet function as - javascript is not yet implemented

---
+++ public/jscripts/index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload')
+const io = require('socket.io')(http);
+const app = express();
+const port = process.env.PORT || 5000;
+const report_dir = 'ScheduledOutput'//'C:\\temp\\ScheduledOutput'
+const host_name = `http://localhost:${port}`
+
+
+// Add your code here
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('../public'));
+//app.use(express.static(__dirname)+'../public'); // serves resources from public folder
+//app.use('/static', express.static(path.join(__dirname, 'public')))
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024 * 1024 },
+}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'../index.html'))
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
